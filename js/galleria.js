09d321acdcb2d3aec0cbf3b170e5d9c3eb189c94
Henna Toivoniemi <henna.toivoniemi@gmail.com>
2024-11-07 14:55:29 +0200

    layout galleriasivulle

---
+++ js/galleria.js
@@ -0,0 +1,35 @@
+const remonttikohteet = [
+    {   kohde:1, 
+        kuvaus:"Vierashuoneeseen maalattiin tehosteseinä sekä kiinteiden kalusteiden maalaustyö", 
+        kesto:"2 työpäivää, yksi tekijä.", 
+        hinta:"Työn osuus 950 € + materiaalikulut 200€ = 1150 €."},
+    
+    {   kohde:2, 
+        kuvaus:"Olohuoneeseen toteutettiin osittainen tehosteseinä", 
+        kesto:"4 tuntia, yksi tekijä.", 
+        hinta:"Työn osuus 250€ + materiaalikulut 80€ = 330 €."},
+    {
+        kohde:3,
+        kuvaus:"Keittiön ja ruokailutilan ilme raikastettiin maalauksella",
+        kesto:"6 tuntia, kaksi tekijää.",
+        hinta:"Työn osuus 720 € + materiaalikulut 150 € = 870 €."
+    },
+    {
+        kohde:4,
+        kuvaus:"Makuuhuoneen vanhan tapetin poisto, tasoitustyöt, uuden tapetin asennus tehosteseinälle ja muiden seinien maalaus.",
+        kesto:"2 työpäivää, kaksi tekijää.",
+        hinta:"Työn osuus 1900 € + materiaalikulut 200 € = 2100 €."
+    },
+    {
+        kohde:5,
+        kuvaus:"Omakotitalon (120m2) koko sisätilojen katon tasoitus ja maalaaminen.",
+        kesto:"8 tuntia, kaksi tekijää",
+        hinta:"Työn osuus 950 € + materiaalikulut 200 € = 1150 €."
+    },
+    {
+        kohde:6,
+        kuvaus:"Makuuhuoneen seinien maalaus uudella värillä.",
+        kesto:"6 tuntia, yksi tekijä",
+        hinta:"Työn osuus 350 € + materiaalikulut 80 € = 430 €."
+    }
+]
