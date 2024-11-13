ebfbdca463e95950b5dc381ec76fbaa79d126b79
Henna Elina Toivoniemi <s4tohe03@students.osao.fi>
2024-11-13 13:58:18 +0200

    hampparivalikko toimii palvelut-sivulla

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,14 @@
-const hamburgerToggle = document.getElementById("hamburger")
+const hamburgerToggle = document.getElementById("hamburgerButton")
 const navbarDivElement = document.getElementById("navbarDiv")
+const hamburgerNavbarElement = document.getElementById("hamburgerNavbar")
 
 hamburgerToggle.addEventListener("click", () => {
     if(navbarDivElement.classList.contains("open")) {
         navbarDivElement.classList.remove("open")
+        hamburgerNavbarElement.style.display = "none"
     } else {
         navbarDivElement.classList.add("open")
+        hamburgerNavbarElement.style.display = "flex"
     }
     console.log('klikkasit hampurilaista')
 
