2d6b2fa60bcdee3a72b6825e41c341ca02553505
Henna Toivoniemi <henna.toivoniemi@gmail.com>
2024-11-14 17:00:38 +0200

    hampurilaisnavigointi toimii palveluissa

--- js/navbar.js
+++ js/navbar.js
@@ -1,15 +1,20 @@
-const hamburgerToggle = document.getElementById("hamburgerButton")
-const navbarDivElement = document.getElementById("navbarDiv")
-const hamburgerNavbarElement = document.getElementById("hamburgerNavbar")
+const hamburgerButton = document.getElementById("hamburgerButton") // tähän tallennettu hampurilaisvalikon nappula
+const closeIcon = "./assets/close-lg-svgrepo-com.svg" // tallennetaan sulkemisikoni muuttujaan
+const hamburgerIcon = "./assets/hamburger-lg-svgrepo-com.svg" //tallennetaan hamppari-ikoni muuttujaan
+const hamburgerSVG = document.getElementById("hamburgerSVG") // tallennetaan alkuperäinen svg-img elementti muuttujaan
+const navbarDivElement = document.getElementById("navbarDiv") // tallennetaan navigoinnin container muuttujaan
+const hamburgerNavbarElement = document.getElementById("hamburgerNavbar") // hampparivalikko muuttujaan
 
-hamburgerToggle.addEventListener("click", () => {
+// lisätään klikkauksen kuuntelija hampurilaisvalikolle
+hamburgerButton.addEventListener("click", () => {
     if(navbarDivElement.classList.contains("open")) {
         navbarDivElement.classList.remove("open")
         hamburgerNavbarElement.style.display = "none"
+        hamburgerSVG.src = hamburgerIcon // käytetään hamppari-ikonia kun valikko on kiinni
     } else {
         navbarDivElement.classList.add("open")
         hamburgerNavbarElement.style.display = "flex"
-    }
-    console.log('klikkasit hampurilaista')
+        hamburgerSVG.src = closeIcon  // vaihdetaan näkyviin X-ikoni, kun valikko on avoinna
 
+    }
 })
